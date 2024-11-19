13b39fd6edea3f2a8cc1f31874c5565c4cf8c4e4
Zabloing <102370841+Zabloing1@users.noreply.github.com>
2024-11-19 17:26:28 +0000

    Update EVENTVIEWER.ps1

--- EVENTVIEWER.ps1
+++ EVENTVIEWER.ps1
@@ -1,3 +1,7 @@
+Clear-Host
+
+Write-Host
+
 $EventIDs = @(4616, 3079, 1102, 7036, 4663)
 $EventLogs = @("Security", "Application", "Security", "Application", "Application", "Application")
 $Results = @()
