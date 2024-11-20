6df6805bada5b5c63f62b93f8fd7af8db057feaf
Zabloing <102370841+Zabloing1@users.noreply.github.com>
2024-11-20 17:47:19 +0000

    Update EVENTVIEWER.ps1

--- EVENTVIEWER.ps1
+++ EVENTVIEWER.ps1
@@ -1,5 +1,5 @@
-$EventIDs = @(4616, 3079, 1102, 7036, 4663)
-$EventLogs = @("Security", "Application", "Security", "Application", "Application", "Application")
+$EventIDs = @(3079, 1102, 7, 7036, 7045, 1001, 1002, 104, 1074, 4663, 7036, 4688, 4660, 4616)
+$EventLogs = @("Application", "System", "Security", "Windows PowerShell", "Windows PowerShell Operational", "Setup", "ForwardedEvents", "Internet Explorer")
 $Results = @()
 foreach ($EventLog in $EventLogs) {
 $Events = foreach ($EventID in $EventIDs) {
@@ -7,4 +7,4 @@ Get-EventLog -LogName $EventLog -InstanceId $EventID -Newest 3
      }
      $Results += $Events
  }
-$Results |  out-gridview
+$Results | out-gridview
