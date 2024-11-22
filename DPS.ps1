00fee206e102c9191167db401476d520a61ba948
Zabloing <102370841+Zabloing1@users.noreply.github.com>
2024-11-22 21:10:15 +0000

    Update DPS.ps1

--- DPS.ps1
+++ DPS.ps1
@@ -31,7 +31,7 @@ if (-not (Test-Path -Path $strings2Path)) {
 }
 
 $outputFilePath = Join-Path $scriptDirectory "dump.txt"
-$dumpCommand = "$strings2path -raw -nh -pid $targetPID"
+$dumpCommand = "$strings2path -raw -nh"
 
 try {
     $memoryDump = Invoke-Expression $dumpCommand
